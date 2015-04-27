d9e0470e9ed2534d3714e0cd3529a9f1a578bf55
rifayath <rifayath.david@gmail.com>
2015-04-27 21:57:17 +0200

    Creation de l'IA moyen

---
+++ Stupide Vautour/Stupide Vautour/players/MediumSmart.cs
@@ -0,0 +1,31 @@
+﻿using Stupide_Vautour.game;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stupide_Vautour.players
+{
+    class MediumSmart : Stupid
+    {
+        public MediumSmart() : base()
+        {
+
+        }
+
+        public override Card play(Card animal)
+        {
+
+            
+            return myCards.getRandomCard();
+        }
+
+        public override Card play(Card cardAnimal, Stack pioche)
+        {
+            int valeurPioche = getValeurCartePioche(cardAnimal, pioche) * 10;
+
+            //return myCards.getRandomCard();
+        }
+    }
+}
